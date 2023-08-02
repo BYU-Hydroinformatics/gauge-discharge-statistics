a6a0f2123ff440c58b676cb1ebc0f605d1de3a90
kayshao <kayshaobrien@gmail.com>
2023-08-02 13:40:56 -0600

    added function to create histogram and boxplots, added code to fix peru_116 but there are still problems with grdc files. once those files are fixed the code needs to be rerun calling function_to_calculate_stats and then function_to_create_graphs needs to be called on the file created from that to create the histograms and boxplots

--- functions.py
+++ functions.py
@@ -14,13 +14,19 @@ def function_to_calculate_stats(str_path_csv_file: str) -> pd.DataFrame:
     pd.DataFrame
         a dataframe with stats for each column in the csv file
     """
-    df = pd.read_csv(str_path_csv_file, index_col=0)
+    dataf = pd.read_csv(str_path_csv_file, index_col=0)
+
+    # make a copy of dataf to work with
+    df = dataf.copy()
 
     # drop possible empty third column
     df = df.dropna(how='all', axis=1)
 
     # drop rows with empty values
     df = df.dropna()
+
+    # drop rows with negative values
+    df = df[df >= 0].dropna()
 
     df.index = pd.to_datetime(df.index)
     # don't return anything if there are no measurements
@@ -73,8 +79,8 @@ def function_to_calculate_stats(str_path_csv_file: str) -> pd.DataFrame:
     calculated_stats['n_nov'] = df[df.index.month == 11].count()
     calculated_stats['n_dec'] = df[df.index.month == 12].count()
 
-    # calculate number of gaps longer than one month
-    calculated_stats['n_gaps'] = df.resample('M').count().isnull().sum()
+    # calculate number of gaps longer than one month using the difference between the index values
+    calculated_stats['n_gaps'] = df.index.to_series().diff().dt.days.gt(31).sum()
 
     # see if most recent measurement is over a year ago
     calculated_stats['last_obs'] = df.index.max()
@@ -107,13 +113,93 @@ def function_to_create_graphs(str_path_csv_file: str) -> None:
     None
     """
     df = pd.read_csv(str_path_csv_file, index_col=0)
+    # convert last observed column to datetime then int
+    df['last_obs'] = pd.to_datetime(df['last_obs'], errors='coerce')
+    df['last_obs'] = df['last_obs'].dt.year
+    # convert float columns to int
+    df['last_year'] = df['last_year'].astype(int)
+    df['first_year'] = df['first_year'].astype(int)
+    # TODO: add in a line that checks for duplicate stream gauge ids and prints them out
+    # TODO: fix peru_116 years
 
-    # create and save a histogram and boxplot for each column
+    # add unit attribute to each column of the dataframe
+    units = ['m3/s', 'm3/s', 'm3/s', 'm3/s', 'm3/s', 'm3/s', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+             ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    tuples = list(zip(df.columns, units))
+    df.columns = pd.MultiIndex.from_tuples(tuples, names=['name', 'units'])
+
     for column in df.columns:
-        df[column].plot.hist(bins=100)
-        plt.savefig(os.path.join('graphs', column + '_histogram.png'))
-        plt.close()
+        column_name = column[0]
+        column_units = column[1]
+        create_histogram(df[column[0]], column_name, column_units)
+        create_boxplot(df[column[0]], column_name, column_units)
+        include_outliers = False
+        create_histogram(df[column[0]], column_name, column_units, include_outliers)
+        create_boxplot(df[column[0]], column_name, column_units, include_outliers)
 
-        df[column].plot.box()
-        plt.savefig(os.path.join('graphs', column + '_boxplot.png'))
-        plt.close()
+
+def create_histogram(df_column, column_name, column_units, include_outliers=True):
+    """
+    create histogram for columns, choosing to include outliers or not
+    parameters:
+        df_column : pd.DataFrame
+            column of data from big dataframe
+        column_name : str
+            name of column
+        column_units : str
+            units of column
+        include_outliers : bool
+            whether to include outliers in the histogram
+
+    returns:
+    None
+    """
+    if not include_outliers:
+        iqr = df_column.quantile(0.75) - df_column.quantile(0.25)
+        df_column = df_column[(df_column < df_column.quantile(0.75) + 1.5 * iqr) & (df_column > df_column.quantile(0.25)
+
+                                                                                    - 1.5 * iqr)]
+    folder_name = 'histograms_with_outliers' if include_outliers else 'histograms_without_outliers'
+    # Create the folder if it doesn't exist
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+    # Generate the file path for saving the histogram
+    file_path = os.path.join(folder_name, f'{column_name}_histogram.png')
+    # Create and save the histogram
+    histogram = df_column.plot.hist(bins=10)
+    plt.title(f'{column_name}{" without outliers" if not include_outliers else ""}')
+    plt.xlabel(f'{column_name} ({column_units})' if column_units != " " else column_name)
+    plt.ylabel('Frequency')
+    plt.bar_label(histogram.containers[0])
+    histogram.legend().remove()
+    plt.savefig(file_path)
+
+
+def create_boxplot(df_column, column_name, column_units, include_outliers=True):
+    """
+    create boxplot for columns, choosing to include outliers or not
+    parameters:
+        df_column : pd.DataFrame
+            column of data from big dataframe
+        column_name : str
+            name of column
+        column_units : str
+            units of column
+        include_outliers : bool
+            whether to include outliers in the boxplot
+
+    returns:
+    None
+    """
+    folder_name = 'boxplots_with_outliers' if include_outliers else 'boxplots_without_outliers'
+    # Create the folder if it doesn't exist
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+    # Generate the file path for saving the boxplot
+    file_path = os.path.join(folder_name, f'{column_name}_boxplot.png')
+    # create and save the boxplot
+    df_column.plot.box(showfliers=include_outliers)
+    plt.title(f'{column_name}{" without outliers" if not include_outliers else ""}')
+    plt.ylabel(f'{column_name} {column_units}')
+    plt.xticks([])
+    plt.savefig(file_path)
--- main.py
+++ main.py
@@ -9,14 +9,44 @@ df = pd.DataFrame()
 for filename in glob.glob('/Users/student/PycharmProjects/gauge-discharge-test-code/observed_discharge/*.csv'):
     if os.path.getsize(filename) > 0:
         csv = os.path.join(directory, filename)
-        print(csv)
         df = pd.concat([df, function.function_to_calculate_stats(csv)])
 
 # sort alphabetically by first column
 df = df.sort_values(by=['gauge_id'])
 
 # save to csv
-df.to_csv('stats.csv')
+df.to_csv('stats_updated.csv')
 
 # create graphs
-function.function_to_create_graphs('stats.csv')
+function.function_to_create_graphs('/Users/student/PycharmProjects/gauge-discharge-statistics/stats_updated.csv')
+print("completed")
+'''
+## edit peru_116.csv dates to be 19xx instead of 20xx for the ones that are wrong
+# read in peru_116.csv to df
+df = pd.read_csv('/Users/student/PycharmProjects/gauge-discharge-statistics/observed_discharge/observed_discharge/peru_116.csv')
+
+# Specify the column containing the years
+year_column = 'Datetime'
+
+# Convert years starting with '20' to start with '19' for a specific number of lines
+line_limit = 8034  # Set the desired number of lines to modify
+
+df_subset = df.head(line_limit).copy()  # Select the specified number of lines and make a copy
+# change datetime column to datetime format
+df_subset[year_column] = pd.to_datetime(df_subset[year_column], format='%Y-%m-%d %H:%M:%S')
+# change datetime column to string format
+df_subset[year_column] = df_subset[year_column].dt.strftime('%Y-%m-%d %H:%M:%S')
+# replace 20 with 19
+df_subset[year_column] = df_subset[year_column].str.replace('20', '19')
+# change datetime column back to datetime format
+df_subset[year_column] = pd.to_datetime(df_subset[year_column], format='%Y-%m-%d %H:%M:%S')
+# change datetime column back to object format
+df_subset[year_column] = df_subset[year_column].dt.strftime('%Y-%m-%d %H:%M:%S')
+
+# Update the modified subset back into the original DataFrame
+df.update(df_subset) # pretty sure this line is the issue
+
+# Save the modified DataFrame to a new CSV file in observed_discharge
+df.to_csv('/Users/student/PycharmProjects/gauge-discharge-statistics/observed_discharge/observed_discharge/peru_116.csv', index=False)
+'''
+# TODO: possible improvement - combine some box plots into one graph - do it in R?
